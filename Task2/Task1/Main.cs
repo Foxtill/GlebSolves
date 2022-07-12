778947b2197bf2a5df0a0679de4cf604519d4b78
Foxtill <53660090+Foxtill@users.noreply.github.com>
2022-07-12 15:47:42 +0300

    Add files via upload

---
+++ Task2/Task1/Main.cs
@@ -0,0 +1,29 @@
+﻿public class Main
+{ 
+    private Soldier soldier = new Soldier();
+    private Gym gym = new Gym();
+    public void Start()
+    {
+        soldier.PumpUp(gym.dumbbell);
+        soldier.PumpUp(gym.barbell);
+        soldier.PumpUp(gym.treadmill);
+        Console.WriteLine(soldier.strength);
+    }
+}
+
+public class Soldier
+{
+    public int strength = 10;
+    public void PumpUp(Dumbbell dumbbell)
+    {
+        strength += dumbbell.strength;
+    }
+    public void PumpUp(Barbell barbell)
+    {
+        strength += barbell.strength;
+    }
+    public void PumpUp(Treadmill treadmill)
+    {
+        strength += treadmill.strength;
+    }
+}
