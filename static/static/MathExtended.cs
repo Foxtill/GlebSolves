0dc623429c20a6a45a1e062a3ce7abcca03848ff
Foxtill <53660090+Foxtill@users.noreply.github.com>
2022-07-15 03:47:04 +0300

    Add files via upload

---
+++ static/static/MathExtended.cs
@@ -0,0 +1,8 @@
+﻿public class MathExtended
+{
+    public static int lastCalculatedValue;
+    public static int Clamp(int value, int min, int max)
+    {
+        return lastCalculatedValue  = value > max ? max : value < min ? min : value;
+    }
+}
---
+++ static/static/Program.cs
@@ -0,0 +1,2 @@
+﻿Console.WriteLine("Clamp value = " + MathExtended.Clamp(2, 3, 5));
+Console.WriteLine("Last calculated value = " + MathExtended.lastCalculatedValue);
